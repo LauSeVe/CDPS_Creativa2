f1ad05553d0058479241d933051325b261762a89
LauSeVe <82331980+LauSeVe@users.noreply.github.com>
2022-01-14 19:01:45 +0100

    Update scriptpc2.py

--- scriptpc2.py
+++ scriptpc2.py
@@ -94,7 +94,7 @@ d=open("productpage_monolith.py","r")
 f=open("auxiliar.py","w")
 for line in d:
     if "app.run(host='::', port=p, debug=True, threaded=True)" in line:
-        f.write("app.run(host=IPAddr, port=p, debug=True, threaded=True)")
+        f.write("app.run(host=IPAddr, port=p, debug=True, threaded=True) \n")
     else:
         f.write(line)
 f.close()
